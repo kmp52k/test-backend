9b0bd2a8ec86d20525a6dfd4b4e9a3af17ca6fcc
Krunal <krunal.purohit01@infosys.com>
2022-03-09 20:49:20 +0530

    Amplify init

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "testbackend": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
